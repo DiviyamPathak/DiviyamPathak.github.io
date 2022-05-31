51f28c417efd26b37dd6f0cbf3ce4c3343fbb77f
DiviyamPathak <patdiviyam@gmail.com>
2022-05-31 13:16:31 +0530

    putting projects into view javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+projectsbtn = document.getElementById("probtn")
+projectsbtn.addEventListener("click" , focusonprojects)
+pojects = document.getElementById("projects")
+function focusonprojects( ){
+    pojects.scrollIntoView(true)
+    console.log("exs")
+}

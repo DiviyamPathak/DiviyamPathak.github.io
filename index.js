52507390a27ce5f6f144595db268c05714d730e2
DiviyamPathak <patdiviyam@gmail.com>
2024-06-28 14:54:10 +0530

    maintainance

--- index.js
+++
@@ -1,7 +0,0 @@
-projectsbtn = document.getElementById("probtn")
-projectsbtn.addEventListener("click" , focusonprojects)
-pojects = document.getElementById("projects")
-function focusonprojects( ){
-    pojects.scrollIntoView(true)
-    console.log("exs")
-}
